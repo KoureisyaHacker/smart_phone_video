1686305301197b6480ca3166b0c51238ebcfc3df
HORIRyoto <c1218923@st.kanazawa-it.ac.jp>
2023-12-15 16:09:50 +0900

    2023年12月15日追加

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ let nav = document.querySelector("#navArea");
 let btn = document.querySelector(".toggle-btn");
 let mask = document.querySelector("#mask");
 
+const menu = document.querySelector(".logo");
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop >= menu.offsetTop) {
+    //stickyクラスをnavbarに付与する。
+    navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  });
+
 btn.onclick = () => {
   nav.classList.toggle("open");
 };
